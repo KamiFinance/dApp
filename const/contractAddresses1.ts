ff342ed5982ab61615abb9d10ca75a75ebf4ba90
KamiFinance <84658738+KamiFinance@users.noreply.github.com>
2023-02-20 13:59:43 +0100

    Add files via upload

---
+++ const/contractAddresses1.ts
@@ -0,0 +1,6 @@
+export const editionDropContractAddress =
+  "0x58f602B9098Ef521E3df4b78965c0201c39D9875";
+export const tokenContractAddress =
+  "0xfCc58E427B9DB852480D09F93a5Fd572986DD24e";
+export const stakingContractAddress =
+  "0xeF6C6F2ca9681786a127444DA87Be32b086BCAf3";
